bb632f758c0eb32e6e656a4ae9a24c7f0c77cfad
osteel <yannickchenot@gmail.com>
2022-02-19 16:16:50 +0000

    upgraded to PHP 8.1/Node 17/Laravel 9 and switched to using Vite

--- src/frontend/src/components/HelloThere.vue
+++ src/frontend/src/components/HelloThere.vue
@@ -1,31 +1,31 @@
 <template>
-  <div>
-    <img src="https://tech.osteel.me/images/2020/03/04/hello.gif" alt="Hello there" class="center">
-    <p>{{ msg }}</p>
-  </div>
+    <div>
+        <img src="https://tech.osteel.me/images/2020/03/04/hello.gif" alt="Hello there" class="center">
+        <p>{{ msg }}</p>
+    </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloThere',
-  props: {
-      msg: String
-  }
+    name: 'HelloThere',
+    props: {
+        msg: String
+    }
 }
 </script>
 
 <style>
 p {
-  font-family: "Arial", sans-serif;
-  font-size: 90px;
-  text-align: center;
-  font-weight: bold;
+    font-family: "Arial", sans-serif;
+    font-size: 90px;
+    text-align: center;
+    font-weight: bold;
 }
 
 .center {
-  display: block;
-  margin-left: auto;
-  margin-right: auto;
-  width: 50%;
+    display: block;
+    margin-left: auto;
+    margin-right: auto;
+    width: 50%;
 }
 </style>
