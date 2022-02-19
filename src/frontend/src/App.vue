924af9d9b5a35efaa2e47fdccbd0afc76a1a5149
osteel <yannickchenot@gmail.com>
2022-02-19 16:16:50 +0000

    upgraded to PHP 8.1/Node 17/Laravel 9 and switched to using Vite

--- src/frontend/src/App.vue
+++ src/frontend/src/App.vue
@@ -1,7 +1,7 @@
 <template>
-  <div id="app">
-    <HelloThere :msg="msg"/>
-  </div>
+    <div id="app">
+      <HelloThere :msg="msg"/>
+    </div>
 </template>
 
 <script>
@@ -9,19 +9,19 @@ import axios      from 'axios'
 import HelloThere from './components/HelloThere.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloThere
-  },
-  data () {
-    return {
-      msg: null
+    name: 'App',
+    components: {
+        HelloThere
+    },
+    data () {
+        return {
+            msg: null
+        }
+    },
+    mounted () {
+        axios
+            .get('https://backend.demo.test/api/hello-there')
+            .then(response => (this.msg = response.data))
     }
-  },
-  mounted () {
-    axios
-      .get('https://backend.demo.test/api/hello-there')
-      .then(response => (this.msg = response.data))
-  }
 }
 </script>
---
+++ src/frontend/vite.config.js
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  server: {
+    host: true,
+    hmr: {port: 443},
+    port: 8080,
+    watch: {
+      usePolling: true
+    }
+  }
+})
